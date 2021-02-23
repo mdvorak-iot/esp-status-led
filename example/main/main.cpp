9283e9f812d8f0929d90ac306ca94584c24b2642
Michal Dvorak <michal@mdvorak.org>
2021-02-23 16:36:10 +0100

    removed //test from main

--- example/main/main.cpp
+++ example/main/main.cpp
@@ -9,7 +9,7 @@ extern "C" void app_main()
 {
     ESP_ERROR_CHECK(status_led_create_default());
 
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == false); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == false);
 
     // Set manual state
     ESP_LOGI(TAG, "Manual");
@@ -37,9 +37,9 @@ extern "C" void app_main()
     vTaskDelay(200 / portTICK_PERIOD_MS); // intentionally less then timeout
 
     ESP_ERROR_CHECK(status_led_set_interval_for(STATUS_LED_DEFAULT, 200, true, 1000, true));
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == true); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == true);
     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == false); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == false);
 
     ESP_ERROR_CHECK(status_led_set_interval_for(STATUS_LED_DEFAULT, 40, true, 1000, false));
     vTaskDelay(2000 / portTICK_PERIOD_MS);
@@ -47,17 +47,17 @@ extern "C" void app_main()
     // Timeout without blinking
     ESP_LOGI(TAG, "Timeout without blinking");
     ESP_ERROR_CHECK(status_led_set_interval_for(STATUS_LED_DEFAULT, 0, true, 1000, false));
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == true); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == true);
     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == false); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == false);
 
     // Manual toggle
     ESP_LOGI(TAG, "Toggle");
     ESP_ERROR_CHECK(status_led_toggle_state(STATUS_LED_DEFAULT)); // off -> on
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == false); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == false);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     ESP_ERROR_CHECK(status_led_toggle_state(STATUS_LED_DEFAULT)); // on -> off
-    assert(status_led_is_active(STATUS_LED_DEFAULT) == false); // test
+    assert(status_led_is_active(STATUS_LED_DEFAULT) == false);
 
     // Cleanup
     ESP_LOGI(TAG, "Cleanup");
